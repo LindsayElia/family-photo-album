42018cf4f7f407d0b1ee554e5373d64c08bfc78b
LindsayElia <lindsay.elia@gmail.com>
2015-09-16 17:40:16 -0700

    first deploy to heroku

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.get("*", function(req,res){
 
 
 //***** START SERVER *****
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Server starting on port: 3000");
 });
 
